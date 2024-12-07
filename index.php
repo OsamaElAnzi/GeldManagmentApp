3f4149582baf36581526b3d6af98d9e3069b4131
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2024-12-07 20:54:20 +0100

    icon toegevoegd aan site

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include 'back-end.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GeldManagmentApp</title>
+    <link type="image/x-icon" rel="icon" href="./foto/profits.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <style>
         .circle-container {
